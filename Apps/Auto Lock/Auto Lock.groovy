1a4f61b52b4e4996287537e3784463ea654dc545
heidrickla <lewis.heidrick@gmail.com>
2021-01-14 12:45:45 -0600

    Update Auto Lock.groovy

--- Apps/Auto Lock/Auto Lock.groovy
+++ Apps/Auto Lock/Auto Lock.groovy
@@ -7,7 +7,7 @@
  *   12/28/2020 - Project Published to GitHub
  */
 def setVersion(){
-	state.version = "1.1.22" // Version number of this app
+	state.version = "1.1.24" // Version number of this app
 	state.InternalName = "Auto Lock"   // this is the name used in the JSON file for this app
 }
 
